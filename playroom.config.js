c0dd9e9c12b1591e75320f63dff07519561723f8
Jonas Engström <jonas.engstrom@digia.com>
2019-04-17 09:37:22 +0300

    Minimum reproducible example

---
+++ playroom.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    components: './src/components',
+    outputPath: './dist/playroom',
+    // Optional:
+    title: 'My Awesome Library',
+
+    widths: [320, 375, 768, 1024],
+    port: 9000,
+    openBrowser: true,
+
+  };
+  
---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                This is only a test
+            </div>
+        );
+    }
+}
+
+export default Test;
